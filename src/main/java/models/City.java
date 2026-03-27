f7768d2ecda7d975f2dd3362443ba982a062ed8c
Andre <andre.guido@uamv.edu.ni>
2026-03-27 16:15:13 -0600

    Add City Class

---
+++ src/main/java/models/City.java
@@ -0,0 +1,49 @@
+package models;
+
+public class City {
+    private String name;
+    private String description;
+    private String code;
+
+    public City() {
+    }
+
+    public City(String name, String description, String code) {
+        this.name = name;
+        this.description = description;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return "Datos de la ciudad \n"
+                + "Nombre: " + this.getName()
+                + "\nDescripción: " + this.getDescription()
+                + "\nCódigo: " + this.getCode()
+                + "\n ======================\n";
+    }
+}
